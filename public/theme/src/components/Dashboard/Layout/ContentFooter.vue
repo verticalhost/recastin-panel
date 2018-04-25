1b88cb3a40ff4d310ba175d1c6d289d48791ae67
Shyim <shyim@posteo.de>
2018-04-25 20:54:08 +0200

    Removed dead footer link

--- public/theme/src/components/Dashboard/Layout/ContentFooter.vue
+++ public/theme/src/components/Dashboard/Layout/ContentFooter.vue
@@ -3,16 +3,12 @@
     <div class="container-fluid">
       <nav>
         <ul class="footer-menu">
-          <li>
-            <router-link :to="{path:'/admin'}">Dashboard</router-link>
-          </li>
         </ul>
       </nav>
       <div class="copyright text-center">
         &copy; Coded with
         <i class="fa fa-heart heart"></i> by
-        <a href="https://github.com/cristijora" target="_blank">Cristi Jora</a>.
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
+        <a href="https://github.com/shyim" target="_blank">Shyim</a>
       </div>
     </div>
   </footer>
